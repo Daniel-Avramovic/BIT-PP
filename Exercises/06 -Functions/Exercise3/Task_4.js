f1da321d0cee718459c16e0a13cda1dd45d61f05
Daniel-Avramovic <danijelgti@gmail.com>
2021-04-18 20:44:27 +0200

    Modified

--- Exercises/06 -Functions/Exercise3/Task_4.js
+++ Exercises/06 -Functions/Exercise3/Task_4.js
@@ -2,15 +2,15 @@
 //Write a function that reverses a number. The result must be a number.
 //12345 -> 54321 // Output must be a number
 
-//function reversDigits(num) {
-//  let rev_num = 0;
-//  while (num > 0) {
-//    rev_num = rev_num * 10 + (num % 10);
-//    num = Math.floor(num / 10);
-//  }
-//  return rev_num;
-//}
-//console.log(reversDigits(12345));
+function reversDigits(num) {
+  var rev_num = 0;
+  while (num > 0) {
+    rev_num = rev_num * 10 + (num % 10);
+    num = parseInt(num / 10);
+  }
+  return rev_num;
+}
+console.log(reversDigits(12345));
 
 var num = 123346;
 function reverse(a) {
--- Notes form Class/04-nested loops.js
+++ Notes form Class/04-nested loops.js
@@ -10,7 +10,7 @@ console.log(res);
 var a = [[1, 2, 1, 24], 
         [8, 11, 9, 4], 
         [7, 0, 7, 27]];
-var result = '';
+var result = [];
   for(var i = 0; i < a.length; i++) {
       var currentElement = a[i];
         for(var j = 0; j < currentElement.length; j++) {
